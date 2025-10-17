9735b7cfe3a0cae1b25e98cc2662eabb29ac3d66
morpheus1358 <emreisikgor1358@gmail.com>
2025-10-17 23:17:42 +0300

    Added README and MIT License

--- Desktop/StocksSocialAPIProject/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ Desktop/StocksSocialAPIProject/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380d86da2f44470ef394173ae447ef683c5e70d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
